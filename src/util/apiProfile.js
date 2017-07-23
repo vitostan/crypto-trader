7ddd34bf0312cf81e5a61c5a4dcae8c146a35370
Vitostan <yllbxx9072@gmail.com>
2017-07-23 18:00:59 +0900

    remove houbi key and secret

--- src/util/apiProfile.js
+++ src/util/apiProfile.js
@@ -1,7 +1,7 @@
 export const HOST = 'https://api.bitflyer.jp';
-export const KEY = '{yourKey}';
-export const SECRET = '{yourSecret}';
+export const KEY_BITFLYER = process.env.KEY_BITFLYER;
+export const SECRET_BITFLYER = process.env.SECRET_BITFLYER;
 
 export const HOST_HUOBI = 'https://be.huobi.com';
-export const KEY_HUOBI = '2a88f863-50c6748a-c4b16d97-fb26d';
-export const SECRET_HUOBI = '8d3a7259-9dc160c7-3ec8650a-e0b29';
+export const KEY_HUOBI = process.env.KEY_HUOBI;
+export const SECRET_HUOBI = process.env.SECRET_HUOBI;
