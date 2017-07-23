923bfa2808090bee778985928efe7a5d89ccb6e2
Vitostan <yllbxx9072@gmail.com>
2017-07-23 19:37:05 +0900

    remove o_o

--- o_o.js
+++
@@ -1,4 +0,0 @@
-export const KEY_BITFLYER = 'SRuasFmzd3a2RwK4HtZhZW';
-export const SECRET_BITFLYER = 'ob0K0iEvt+RJM4F4qUCrPL6zFdqaQPJagJ7WD9ej64s=';
-export const KEY_HUOBI = '2a88f863-50c6748a-c4b16d97-fb26d';
-export const SECRET_HUOBI = '8d3a7259-9dc160c7-3ec8650a-e0b29';
