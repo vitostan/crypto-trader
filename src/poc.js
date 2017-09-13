f7a6efd98c38aedfca5db5ab180e5edf9b501361
Vitostan <yllbxx9072@gmail.com>
2017-09-13 11:55:17 +0900

    study boundary values

--- src/poc.js
+++ src/poc.js
@@ -23,8 +23,31 @@ var poc = async function() {
   //   qs: qs
   // };
   // let result = await request(getReqParam);
-  callApi(REQ_TYPE.PUB_GET, 'a', 'b')
-    // console.log('result = ', result);
+  // callApi(REQ_TYPE.PUB_GET, 'a', 'b')
+  // console.log('result = ', result);
+  let maxInt = Number.MAX_SAFE_INTEGER;
+  let minInt = Number.MIN_SAFE_INTEGER;
+  console.log(maxInt);
+  console.log(maxInt.toString(2).length);
+  console.log(minInt);
+  console.log(minInt.toString(2).length);
+  console.log(maxInt + 1);
+  console.log((maxInt + 1) == (maxInt + 2));
+  let maxValue = Number.MAX_VALUE;
+  let minValue = Number.MIN_VALUE;
+  console.log('maxValue =', maxValue); //2^53-1
+  console.log('minValue =', minValue);
+  console.log('maxValue + 1 =', maxValue + 1);
+  console.log('-maxValue - 1 =', -maxValue - 1);
+  console.log('minValue / 2 =', minValue / 2);
+  let positiveInfinity = Number.POSITIVE_INFINITY;
+  let negativeInfinity = Number.NEGATIVE_INFINITY;
+  console.log('positiveInfinity =', positiveInfinity);
+  console.log('negativeInfinity =', negativeInfinity);
+  console.log('positiveInfinity + 1 =', positiveInfinity);
+  console.log('negativeInfinity - 1 =', negativeInfinity);
+  console.log('positiveInfinity > maxValue ?', positiveInfinity > maxValue);
+  console.log('negativeInfinity < -maxValue ?', negativeInfinity < -maxValue);
 }
 
 poc();
